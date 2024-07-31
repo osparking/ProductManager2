c96f8903e527231b6038378ba6bca75295b9f82b
osparking <jbpark03@gmail.com>
2024-07-31 20:57:13 +0900

    two login method project - datasource

---
+++ src/test/java/space/bum/social/SocialFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
