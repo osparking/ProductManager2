b5d9370e84020c002cffb9568579a2d72368c057
osparking <jbpark03@gmail.com>
2024-07-31 22:50:37 +0900

    live reload and new product save feature

--- src/main/java/space/bum/social/controller/AppController.java
+++ src/main/java/space/bum/social/controller/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import space.bum.social.entity.Product;
 import space.bum.social.service.ProductService;
@@ -28,5 +29,11 @@ public class AppController {
     List<Product> products = service.listAll();
     model.addAttribute("products", products);
     return "index";
+  }
+  
+  @PostMapping("/save") 
+  public String saveProduct (Product product){
+      service.save(product);
+      return "redirect:/";
   }
 }
