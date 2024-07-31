2fa6388baf6eaa167c2908858131ea94bb252d6b
osparking <jbpark03@gmail.com>
2024-07-31 23:09:46 +0900

    thymeleaf currency display feature used

--- src/main/java/space/bum/social/entity/Product.java
+++ src/main/java/space/bum/social/entity/Product.java
@@ -1,9 +1,10 @@
 package space.bum.social.entity;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Transient;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,14 +18,5 @@ public class Product {
   private String name;
   private String brand;
   private String madein;
-  private float price;  
-  public String getPriceStr() {
-    return String.format("%,.0f\n", price);
-  }
-  
-  @Transient
-  private String priceStr;
-  public void setPriceStr(String priceStr) {
-    price = Float.parseFloat(priceStr);
-  }
+  private BigDecimal price;
 }
