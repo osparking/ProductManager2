68a22659417e61517e21575a735581cad63af4cd
osparking <jbpark03@gmail.com>
2024-07-31 22:53:47 +0900

    product price 0 error fixed

--- src/main/java/space/bum/social/entity/Product.java
+++ src/main/java/space/bum/social/entity/Product.java
@@ -3,6 +3,7 @@ package space.bum.social.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,5 +20,11 @@ public class Product {
   private float price;  
   public String getPriceStr() {
     return String.format("%,.0f\n", price);
+  }
+  
+  @Transient
+  private String priceStr;
+  public void setPriceStr(String priceStr) {
+    price = Float.parseFloat(priceStr);
   }
 }
