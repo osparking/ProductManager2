40f3e31f345f4fe4ff136e66a421cd964d44069a
osparking <jbpark03@gmail.com>
2024-08-01 00:17:42 +0900

    korean currency symbol shown for price

--- src/main/java/space/bum/social/entity/Product.java
+++ src/main/java/space/bum/social/entity/Product.java
@@ -1,10 +1,12 @@
 package space.bum.social.entity;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,4 +21,12 @@ public class Product {
   private String brand;
   private String madein;
   private BigDecimal price;
+  @Transient
+  private String priceStr;
+  public String getPriceStr() {
+    NumberFormat currency = NumberFormat.getCurrencyInstance();
+    priceStr = currency.format(price);
+    return priceStr;
+  }
+  
 }
